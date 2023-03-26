db0a89a802aa733eb232d0e503f83858bfd3e580
Farhan Tahmid <farhantahmid881@gmail.com>
2023-03-26 10:51:16 +0600

    added types and organizations in db

--- Backend-Web/parent_organization/admin.py
+++ Backend-Web/parent_organization/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Organization_type,Parent_organization
 # Register your models here.
+
+@admin.register(Organization_type)
+class Organization_type(admin.ModelAdmin):
+    '''Registering Organization Type in admin panel'''
+    list_display=['id','type']
+
+@admin.register(Parent_organization)
+class Parent_organization(admin.ModelAdmin):
+    '''Registering Parent Organizations in Model Admin'''
+    list_display=['id','name','type']
+
+
+
--- Backend-Web/parent_organization/models.py
+++ Backend-Web/parent_organization/models.py
@@ -3,18 +3,24 @@ from django.urls import reverse
 
 # Create your models here.
 
-class organization_type(models.Model):
+class Organization_type(models.Model):
     '''Defines the type of organizations'''
     type=models.CharField(null=False,blank=False,max_length=30)
     
+    class Meta:
+        verbose_name="Organization Type"
+    def __str__(self) -> str:
+        return self.type
+    def get_absolute_url(self):
+        return reverse("organization_type", kwargs={"type":self.type})
 
 
-class parent_organization(models.Model):
+class Parent_organization(models.Model):
     '''Defines the parent organizations. The users are going to be affiliated with this'''
     '''The primary keys will be auto generated integer values'''
     
     name=models.CharField(null=False,blank=False,max_length=100)
-    type=models.ForeignKey(organization_type,null=False,blank=False,on_delete=models.CASCADE)
+    type=models.ForeignKey(Organization_type,null=False,blank=False,on_delete=models.CASCADE)
     
     class Meta:
         verbose_name="Parent Organization"
