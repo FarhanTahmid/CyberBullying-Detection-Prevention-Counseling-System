8bc28a377aea6e95c9d81cdbfc282a711834aeb9
Farhan Tahmid <farhantahmid881@gmail.com>
2023-03-26 13:17:46 +0600

    created organization users db

--- Backend-Web/parent_organization/models.py
+++ Backend-Web/parent_organization/models.py
@@ -28,3 +28,29 @@ class Parent_organization(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse("parent_organization", kwargs={"name":self.name})
+
+class Parent_organization_users(models.Model):
+    '''
+    Stores the user informations of parent organization's users
+    -uses their user_id as primary key
+    -foreignKey: Parent_organization_id, to solely identify which organization the user belongs to
+    -stores the necessary basic informations needed for a profile
+    '''
+    
+    user_id=models.CharField(null=False,blank=False,primary_key=True,max_length=50)
+    organization_id=models.ForeignKey(Parent_organization,null=False,blank=False,on_delete=models.CASCADE)
+    full_name=models.CharField(null=False,blank=False,max_length=100)
+    birth_date=models.DateField(null=True,blank=True)
+    contact_no=models.CharField(null=True,blank=True,max_length=20)
+    email_address=models.EmailField(null=True,blank=True)
+    home_address=models.CharField(null=True,blank=True,max_length=200)
+    gender=models.CharField(null=True,blank=True,max_length=10)
+    
+    
+    class Meta:
+        verbose_name="Organization Registered User"
+    def __str__(self) -> str:
+        return self.user_id
+    def get_absolute_url(self):
+        return reverse("user", kwargs={"user_id": self.user_id})
+    
