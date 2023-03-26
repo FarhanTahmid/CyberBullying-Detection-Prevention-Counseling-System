5d5858c40191492d62c4e7390bbd782f41e494e0
Farhan Tahmid <farhantahmid881@gmail.com>
2023-03-26 22:54:02 +0600

    users app created

--- Backend-Web/System_To_Detect_Prevent_Cyberbullying_and_Mental_Counseling/settings.py
+++ Backend-Web/System_To_Detect_Prevent_Cyberbullying_and_Mental_Counseling/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parent_organization'
+    'parent_organization',
+    'application_users',
+    
 ]
 
 MIDDLEWARE = [
