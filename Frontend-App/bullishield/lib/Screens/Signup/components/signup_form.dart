9e834f173371afe703e6f3819acc5166b9014665
Farhan Tahmid <farhantahmid881@gmail.com>
2023-03-29 19:08:34 +0600

    new push

--- Frontend-App/bullishield/lib/Screens/Signup/components/signup_form.dart
+++ Frontend-App/bullishield/lib/Screens/Signup/components/signup_form.dart
@@ -4,10 +4,24 @@ import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   const SignUpForm({
     Key? key,
   }) : super(key: key);
+  @override
+  State<SignUpForm> createState() => SignupFormState();
+}
+
+class SignupFormState extends State<SignUpForm> {
+  // Defining Controllers
+  final user_id_controller = TextEditingController();
+  final email_controller = TextEditingController();
+  final password = TextEditingController();
+  final confirm_password = TextEditingController();
+
+  bool signup()async{
+    var response=await
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,36 +42,35 @@ class SignUpForm extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child:TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            cursorColor: kPrimaryColor,
-            onSaved: (email) {},
-            decoration: InputDecoration(
-              hintText: "Your email",
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.email),
-              ),
-            ),
-          ),
-          ),
-        
-          TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
+              onSaved: (email) {},
               decoration: InputDecoration(
-                hintText: "Your password",
+                hintText: "Your email",
                 prefixIcon: Padding(
                   padding: const EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
+                  child: Icon(Icons.email),
                 ),
               ),
             ),
+          ),
+          TextFormField(
+            textInputAction: TextInputAction.done,
+            obscureText: true,
+            cursorColor: kPrimaryColor,
+            decoration: InputDecoration(
+              hintText: "Your password",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.lock),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child:TextFormField(
+            child: TextFormField(
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: kPrimaryColor,
