c3a1331a37801740eb582d02f4ed5dbe967d204f
Farhan Tahmid <farhantahmid881@gmail.com>
2023-04-01 15:32:46 +0600

    created organisational signup

--- Backend-Web/api/views.py
+++ Backend-Web/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
+from application_users.models import Parent_organization_users
 from django.contrib.auth.hashers import make_password
 
 class Signup(APIView):
@@ -13,14 +14,22 @@ class Signup(APIView):
 
         # Check if the username and email are already taken
         if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
-            return Response({'error': 'Username or email already exists','status':400}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Username or email already exists'}, status=status.HTTP_400_BAD_REQUEST)
         
-        # Create a new user
-        user = User.objects.create(
-            username=username,
-            email=email,
-            password=make_password(password)
-        )
-        user.save()
-        return Response({'success': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        # Create a new user if only a member is registered in the organisation database
+        try:
+            Parent_organization_users.objects.get(user_id=username)
+            user = User.objects.create(
+                username=username,
+                email=email,
+                password=make_password(password)
+            )
+            user.save()
+            return Response({'success': 'User created successfully'}, status=status.HTTP_201_CREATED)
 
+        except:
+            Parent_organization_users.DoesNotExist
+            return Response({'error': 'Username Does not exist in the Organisation Database!'}, status=status.HTTP_205_RESET_CONTENT)
+
+
+        
