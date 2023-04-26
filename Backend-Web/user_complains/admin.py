ddf10dd3d73f2519d6f56279e6e72c4f4fc99f88
Farhan Tahmid <farhantahmid881@gmail.com>
2023-04-26 14:49:58 +0600

    api for complaing registering created

--- Backend-Web/user_complains/admin.py
+++ Backend-Web/user_complains/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import User_Complains
 # Register your models here.
+
+@admin.register(User_Complains)
+class User_Complains(admin.ModelAdmin):
+    '''Registering user complains in django admin panel'''
+    list_display=['complainee_id']
--- Backend-Web/user_complains/models.py
+++ Backend-Web/user_complains/models.py
@@ -8,14 +8,17 @@ class User_Complains(models.Model):
     '''
     Stores all the necessary informations for user complains
     '''
+    
+    #complainee_id is the username of the user who is lodging the complain
     complainee_id=models.ForeignKey(Parent_organization_users,null=False,blank=False,on_delete=models.CASCADE)
+    #organization_id is the id of the organization from which the user belongs
     organization_id=models.ForeignKey(Parent_organization,null=False,blank=False,on_delete=models.CASCADE)    
     complain_type=models.CharField(null=False,blank=False,max_length=30,default="Harassment")
     bully_name=models.CharField(null=False,blank=False,max_length=50,default="Unknown")
     bully_id=models.CharField(null=True,blank=True,max_length=30)
     bully_picture=models.ImageField(null=True,blank=True,upload_to='bully_pictures/')    
     incident_date=models.DateField(null=True,blank=True)
-    complain_description=models.TextField(null=False,blank=False,max_length=100)
+    complain_description=models.TextField(null=False,blank=False,max_length=1000)
     complain_status=models.CharField(null=False,blank=False,default="Processing",max_length=30)
     complain_decision=models.CharField(null=True,blank=True,max_length=500)
     bully_guilty=models.BooleanField(null=True,blank=True,default=False)
