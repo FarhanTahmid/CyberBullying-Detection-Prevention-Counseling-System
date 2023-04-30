54b85eeae47cf872bf25022f007f364eeae83b37
Farhan Tahmid <farhantahmid881@gmail.com>
2023-04-30 11:41:50 +0600

    homepage stateful to stateless widget

--- Frontend-App/bullishield/lib/main.dart
+++ Frontend-App/bullishield/lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
           )),
       debugShowCheckedModeBanner: false,
       routes: {
-        "/": (context) => HomePage(),
+        "/": (context) => LoginScreen(),
       },
     );
   }
