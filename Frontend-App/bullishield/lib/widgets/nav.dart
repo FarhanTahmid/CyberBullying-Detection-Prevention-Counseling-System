37e15b892ced7f71e4452e513cc38bc6e061c548
farhanaakbar <farhanaakbar10@gmail.com>
2023-04-30 11:23:44 +0600

    frontend of homepage done

---
+++ Frontend-App/bullishield/lib/widgets/nav.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = "https://avatars.githubusercontent.com/u/96833618?v=4";
+    return Drawer(
+      child: Container(
+        color: Colors.purple.shade100,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.purpleAccent),
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                  accountName: Text("Farhana Akbar"),
+                  accountEmail: Text("farhana.akbar@northsouth.edu"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageUrl),
+                  )),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.purple.shade900,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.purple.shade900,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.purple.shade900,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.purple.shade900,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.bell_circle,
+                color: Colors.purple.shade900,
+              ),
+              title: Text(
+                "Notifications",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.purple.shade900,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
